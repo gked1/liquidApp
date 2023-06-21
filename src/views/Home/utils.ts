7f3787315b0e45c89f71e7f21ee5ba769da29337
Grzegorz (Greg) Kędzior <41132538+gked1@users.noreply.github.com>
2023-06-21 23:05:11 +0200

    Refactor utils for Home component

--- src/views/Home/utils.ts
+++ src/views/Home/utils.ts
@@ -15,4 +15,38 @@ const fetchData = (
   })();
 };
 
-export { fetchData };
+function descendingComparator(a: any, b: any, orderBy: string) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order: string, orderBy: string) {
+  return order === 'desc'
+    ? (a: unknown, b: unknown) => descendingComparator(a, b, orderBy)
+    : (a: unknown, b: unknown) => -descendingComparator(a, b, orderBy);
+}
+
+// function stableSort(array: unknown, comparator: unknown) {
+//   const stabilizedThis = array.map((el: unknown, index: unknown) => [el, index]);
+//   stabilizedThis.sort((a: unknown, b: unknown) => {
+//     const order = comparator(a[0], b[0]);
+//     if (order !== 0) return order;
+//     return a[1] - b[1];
+//   });
+//   return stabilizedThis.map((el: unknown) => el[0]);
+// }
+
+type HeadCells = {
+  id: 'isFavourite',
+  numeric: false,
+  disablePadding: true,
+  label: 'Favourite'
+};
+
+export { fetchData, getComparator };
+export type { HeadCells };
